39846b3930d1a2be24e6b8cd373172d7d93e78de
Nicholas Lukman <nicholaslukman1@gmail.com>
2022-08-27 21:49:49 +1000

    endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 
 
 from flask import Flask, render_template, request
+import psycopg2
 
 app = Flask(__name__)
+conn = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host = '0.0.0.0', port = 5432)
 
 @app.route('/')
 def index():
