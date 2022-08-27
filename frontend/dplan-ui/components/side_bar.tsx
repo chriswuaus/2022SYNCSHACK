639df4025b07cc11cb1c8044e030be44b97ef992
chriswuaus <c.wu_123@hotmail.com>
2022-08-27 23:44:09 +1000

    complete components

--- frontend/dplan-ui/components/side_bar.tsx
+++ frontend/dplan-ui/components/side_bar.tsx
@@ -1,15 +1,27 @@
-import { SideBarProps } from "./components.types"
-import styles from "../styles/SideBar.module.css"
-import Vercel from "../public/vercel.svg"
+import { SideBarProps } from "./components.types";
+import styles from "../styles/SideBar.module.css";
+import Vercel from "../public/vercel.svg";
+import Butt from "./butt";
+import Search from "./search";
 
 const Link = ({}) => {
-    return <Vercel />
-}
+  return <Vercel />;
+};
 
 const SideBar = (props: SideBarProps) => {
-    return <aside className={styles.aside}>
-        <Link/>
+  return (
+    <aside className={styles.aside}>
+      <Link />
+      <div className={styles.home}>
+        <div className={styles.butt}>
+          <Butt highlighted={true} link="asdf" />
+        </div>
+        <div className={styles.search}>
+          <Search highlighted={false} link="asdf" />
+        </div>
+      </div>
     </aside>
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
