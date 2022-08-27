562c55db6654b7967753527100590cf1859e1eff
chriswuaus <c.wu_123@hotmail.com>
2022-08-27 20:05:49 +1000

    add unit card component

---
+++ frontend/dplan-ui/components/plus.tsx
@@ -0,0 +1,6 @@
+import styles from "../styles/Plus.module.css";
+
+const Plus = () => {
+  return <button className={styles.button}>+</button>;
+};
+export default Plus;
--- frontend/dplan-ui/pages/index.tsx
+++ frontend/dplan-ui/pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import UnitCard from "../components/unit_card";
+import Plus from "../components/plus";
+
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <UnitCard unitName="Prog" unitCode="COMP2017" unitRating={5} />
+      <UnitCard
+        unitName="Systems Programming"
+        unitCode="COMP2017"
+        unitRating={5}
+      />
+      <Plus />
     </div>
   );
 };
